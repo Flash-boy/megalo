3a350c5de7ba8e531d0eb4905d6df1b6149ae059
Flash-boy <1944757434@qq.com>
2021-11-24 15:41:13 +0800

    finish config module

--- megalo/config.h
+++ megalo/config.h
@@ -1,22 +1,39 @@
 #ifndef _MEGALO_CONFIG_H_
 #define _MEGALO_CONFIG_H_
 
+#include <cctype>
+#include <list>
 #include <memory>
 #include <stdexcept>
 #include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
 #include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <map>
+#include <utility>
+#include <functional>
 #include <boost/lexical_cast.hpp>
+#include <yaml-cpp/node/node.h>
+#include <yaml-cpp/node/parse.h>
+#include <yaml-cpp/node/type.h>
+#include <yaml-cpp/yaml.h>
+
 #include "log.h"
 
 
 namespace megalo{
 
+// 配置值基类
 class ConfigVarBase{
 public:
   typedef std::shared_ptr<ConfigVarBase> ptr;
   ConfigVarBase(const std::string& name,const std::string& description = "")
     :m_name(name)
     ,m_description(description){
+      std::transform(m_name.begin(), m_name.end(), m_name.begin(), ::tolower);
   }
   virtual ~ConfigVarBase(){}
 
@@ -25,16 +42,239 @@ public:
 
   virtual std::string toString() = 0;
   virtual bool fromString(const std::string& val) = 0;
+  virtual std::string getTypeName() const = 0;
 
 private:
   std::string m_name;
   std::string m_description;
 };
 
-template <class T>
+
+//自定义类型转化
+template<class F, class T>
+class LexicalCast{
+public:
+  T operator()(const F& v){
+    return boost::lexical_cast<T>(v);
+  }
+};
+
+// 类型模板转化偏特化（YAML string 转化为 vector<T>）
+template<class T>
+class LexicalCast<std::string, std::vector<T> >{
+public:
+  std::vector<T> operator()(const std::string& v){
+    YAML::Node node = YAML::Load(v);
+    typename std::vector<T> vec;
+    std::stringstream ss;
+    for(size_t i = 0; i < node.size(); ++i){
+      ss.str("");
+      ss << node[i];
+      vec.push_back(LexicalCast<std::string,T>()(ss.str()));
+    }
+    return vec;
+  }
+};
+
+// 类型模板偏特化（vector<T> 转化为 YAML string）
+template<class T>
+class LexicalCast<std::vector<T>, std::string>{
+public:
+  std::string operator()(const std::vector<T>& v){
+    YAML::Node node(YAML::NodeType::Sequence);
+    for(auto& i : v){
+      node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
+    }
+    std::stringstream ss;
+    ss << node;
+    return ss.str();
+  }
+};
+
+
+// 类型模板转化偏特化（YAML string 转化为 list<T>）
+template<class T>
+class LexicalCast<std::string, std::list<T> >{
+public:
+  std::list<T> operator()(const std::string& v){
+    YAML::Node node = YAML::Load(v);
+    typename std::list<T> vec;
+    std::stringstream ss;
+    for(size_t i = 0; i < node.size(); ++i){
+      ss.str("");
+      ss << node[i];
+      vec.push_back(LexicalCast<std::string,T>()(ss.str()));
+    }
+    return vec;
+  }
+};
+
+
+// 类型模板偏特化（list<T> 转化为 YAML string）
+template<class T>
+class LexicalCast<std::list<T>, std::string>{
+public:
+  std::string operator()(const std::list<T>& v){
+    YAML::Node node(YAML::NodeType::Sequence);
+    for(auto& i : v){
+      node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
+    }
+    std::stringstream ss;
+    ss << node;
+    return ss.str();
+  }
+};
+
+
+// 类型模板转化偏特化（YAML string 转化为 set<T>）
+template<class T>
+class LexicalCast<std::string, std::set<T> >{
+public:
+  std::set<T> operator()(const std::string& v){
+    YAML::Node node = YAML::Load(v);
+    typename std::set<T> vec;
+    std::stringstream ss;
+    for(size_t i = 0; i < node.size(); ++i){
+      ss.str("");
+      ss << node[i];
+      vec.insert(LexicalCast<std::string,T>()(ss.str()));
+    }
+    return vec;
+  }
+};
+
+// 类型模板偏特化（set<T> 转化为 YAML string）
+template<class T>
+class LexicalCast<std::set<T>, std::string>{
+public:
+  std::string operator()(const std::set<T>& v){
+    YAML::Node node(YAML::NodeType::Sequence);
+    for(auto& i : v){
+      node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
+    }
+    std::stringstream ss;
+    ss << node;
+    return ss.str();
+  }
+};
+
+
+
+// 类型模板转化偏特化（YAML string 转化为 unordered_set<T>）
+template<class T>
+class LexicalCast<std::string, std::unordered_set<T> >{
+public:
+  std::unordered_set<T> operator()(const std::string& v){
+    YAML::Node node = YAML::Load(v);
+    typename std::unordered_set<T> vec;
+    std::stringstream ss;
+    for(size_t i = 0; i < node.size(); ++i){
+      ss.str("");
+      ss << node[i];
+      vec.insert(LexicalCast<std::string,T>()(ss.str()));
+    }
+    return vec;
+  }
+};
+
+// 类型模板偏特化（unordered_set<T> 转化为 YAML string）
+template<class T>
+class LexicalCast<std::unordered_set<T>, std::string>{
+public:
+  std::string operator()(const std::unordered_set<T>& v){
+    YAML::Node node(YAML::NodeType::Sequence);
+    for(auto& i : v){
+      node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
+    }
+    std::stringstream ss;
+    ss << node;
+    return ss.str();
+  }
+};
+
+
+
+// 类型模板转化偏特化（YAML string 转化为 map<std::string, T>）
+template<class T>
+class LexicalCast<std::string, std::map<std::string, T> >{
+public:
+  std::map<std::string, T> operator()(const std::string& v){
+    YAML::Node node = YAML::Load(v);
+    typename std::map<std::string, T> vec;
+    std::stringstream ss;
+    for(auto it = node.begin();
+        it != node.end(); ++it){
+      ss.str("");
+      ss << it->second;
+      vec.insert(std::make_pair(it->first.Scalar(),
+                LexicalCast<std::string,T>()(ss.str())));
+    }
+    return vec;
+  }
+};
+
+// 类型模板偏特化（map<std::string,T> 转化为 YAML string）
+template<class T>
+class LexicalCast<std::map<std::string, T>, std::string>{
+public:
+  std::string operator()(const std::map<std::string, T>& v){
+    YAML::Node node(YAML::NodeType::Map);
+    for(auto& i : v){
+      node[i.first] = YAML::Load(LexicalCast<T,std::string>()(i.second));
+    }
+    std::stringstream ss;
+    ss << node;
+    return ss.str();
+  }
+};
+
+
+
+// 类型模板转化偏特化（YAML string 转化为 unordered_map<std::string, T>）
+template<class T>
+class LexicalCast<std::string, std::unordered_map<std::string, T> >{
+public:
+  std::unordered_map<std::string, T> operator()(const std::string& v){
+    YAML::Node node = YAML::Load(v);
+    typename std::unordered_map<std::string, T> vec;
+    std::stringstream ss;
+    for(auto it = node.begin();
+        it != node.end(); ++it){
+      ss.str("");
+      ss << it->second;
+      vec.insert(std::make_pair(it->first.Scalar(),
+                LexicalCast<std::string,T>()(ss.str())));
+    }
+    return vec;
+  }
+};
+
+// 类型模板偏特化（unordered_map<std::string,T> 转化为 YAML string）
+template<class T>
+class LexicalCast<std::unordered_map<std::string, T>, std::string>{
+public:
+  std::string operator()(const std::unordered_map<std::string, T>& v){
+    YAML::Node node(YAML::NodeType::Map);
+    for(auto& i : v){
+      node[i.first] = YAML::Load(LexicalCast<T,std::string>()(i.second));
+    }
+    std::stringstream ss;
+    ss << node;
+    return ss.str();
+  }
+};
+
+
+// T 值类型
+// FromStr 从字符串转换为T类型值的函数对象
+// ToStr 从T类型对象转换为字符串的函数对象
+template <class T, class FromStr = LexicalCast<std::string, T>
+                  ,class ToStr = LexicalCast<T,std::string> >
 class ConfigVar: public ConfigVarBase{
 public:
   typedef std::shared_ptr<ConfigVar<T> > ptr;
+  typedef std::function<void (const T& old_value, const T& new_value)> on_change_cb;
+
   ConfigVar(const std::string& name
            ,const T& default_val
            ,const std::string& description = "")
@@ -44,7 +284,7 @@ public:
 
   std::string toString() override {
     try {
-      return boost::lexical_cast<std::string>(m_val);
+      return ToStr()(m_val);
     } catch(boost::bad_lexical_cast& e){
       MEGALO_LOG_ERROR(MEGALO_LOG_ROOT()) << "ConfigVar::toString exception"
         <<  e.what() << " convert: " << typeid(m_val).name() << " to string";
@@ -53,7 +293,7 @@ public:
   }
   bool fromString(const std::string& val) override {
     try {
-      m_val = boost::lexical_cast<T>(val);
+      setValue(FromStr()(val));
     } catch(boost::bad_lexical_cast& e){
       MEGALO_LOG_ERROR(MEGALO_LOG_ROOT()) << "ConfigVar::fromString exception"
         <<  e.what() << " convert: string to " << typeid(val).name();
@@ -62,9 +302,31 @@ public:
     return true;
   }
   const T getValue() const {return m_val;}
-  void setValue(const T& v) { m_val = v;}
+
+  void setValue(const T& v){
+    if(m_val == v){
+      return;
+    }else{
+      for(auto& i : m_cbs){
+        i.second(m_val, v);
+      }
+    }
+    m_val = v;
+  } 
+
+  std::string getTypeName() const override {return typeid(m_val).name();}
+  void addListener(uint64_t key, on_change_cb cb) {m_cbs[key] = cb;}
+  void delListener(uint64_t key) {m_cbs.erase(key);}
+
+  on_change_cb getListener(uint64_t key) const {
+    auto it = m_cbs.find(key);
+    return it == m_cbs.end() ? nullptr : it->second;
+  }
+  void clearListener() {m_cbs.clear();}
 private:
   T m_val;
+  //配置变更回调函数 
+  std::map<uint64_t, on_change_cb> m_cbs;
 };
 
 class Config{
@@ -73,10 +335,16 @@ public:
   template<class T>
   static typename ConfigVar<T>::ptr Lookup(const std::string& name, 
         const T& default_val, const std::string& description = ""){
-    auto tmp = Lookup<T>(name);
-    if(tmp){
-      MEGALO_LOG_INFO(MEGALO_LOG_ROOT()) << "Lookup name=" << name << " exists";
-      return tmp;
+    auto it = s_datas.find(name);
+    if(it != s_datas.end()){
+      auto tmp = std::dynamic_pointer_cast<ConfigVar<T> >(it->second);
+      if(tmp){
+        MEGALO_LOG_INFO(MEGALO_LOG_ROOT()) << "Lookup name=" << name << " exists";
+        return tmp;
+      }else{
+        MEGALO_LOG_ERROR(MEGALO_LOG_ROOT()) << "Lookup name=" << name << " exists but type not " 
+                << typeid(T).name() << " real_type=" << it->second->getTypeName();
+      }
     }
     if(name.find_first_not_of("abcdefghijklmnopqrstuvwxyz_.0123456789")
         != std::string::npos){
@@ -97,6 +365,12 @@ public:
     }
     return std::dynamic_pointer_cast<ConfigVar<T> >(it->second);
   }
+
+
+  static ConfigVarBase::ptr LookupBase(const std::string& name);
+
+  static void LoadFromYaml(const YAML::Node& root);
+
 private:
   static ConfigVarMap s_datas;
 };
--- tests/test_config.cc
+++ tests/test_config.cc
@@ -1,10 +1,203 @@
 #include "../megalo/config.h"
 #include "../megalo/log.h"
 
-megalo::ConfigVar<int>::ptr g_config_int_value = 
-  megalo::Config::Lookup<int>("system.port",(int)8080,"system port");
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <yaml-cpp/node/node.h>
+#include <yaml-cpp/node/parse.h>
+#include <yaml-cpp/node/type.h>
+#include <yaml-cpp/yaml.h>
 
+megalo::ConfigVar<int>::ptr g_config_int_value = 
+  megalo::Config::Lookup("system.port",(int)8080,"system port");
+
+//megalo::ConfigVar<float>::ptr g_config_xint_value = 
+//  megalo::Config::Lookup("system.port",(float)8080,"system port");
+
+megalo::ConfigVar<float>::ptr g_config_float_value = 
+  megalo::Config::Lookup("system.value",(float)10.2f,"system value");
+
+megalo::ConfigVar<std::vector<int>>::ptr g_config_int_vec_value = 
+  megalo::Config::Lookup("system.int_vec", std::vector<int>{1,2},"system int vec");
+
+megalo::ConfigVar<std::list<int>>::ptr g_config_int_list_value = 
+  megalo::Config::Lookup("system.int_list", std::list<int>{1,2},"system int list");
+
+megalo::ConfigVar<std::set<int>>::ptr g_config_int_set_value = 
+  megalo::Config::Lookup("system.int_set", std::set<int>{1,2},"system int set");
+
+megalo::ConfigVar<std::unordered_set<int>>::ptr g_config_int_uset_value = 
+  megalo::Config::Lookup("system.int_uset", std::unordered_set<int>{1,2},"system int uset");
+
+megalo::ConfigVar<std::map<std::string, int>>::ptr g_config_str_int_map_value = 
+  megalo::Config::Lookup("system.str_int_map", std::map<std::string, int>{{"one",1},{"two",2}},"system str_int map");
+
+megalo::ConfigVar<std::unordered_map<std::string, int>>::ptr g_config_str_int_umap_value = 
+  megalo::Config::Lookup("system.str_int_umap", std::unordered_map<std::string, int>{{"one",1},{"two",2}},"system str_int umap");
+
+void print_yaml(const YAML::Node& node, int level){
+  if(node.IsScalar()){
+    MEGALO_LOG_INFO(MEGALO_LOG_ROOT()) << std::string(level*4,' ') << node.Scalar() << " - " << node.Type() << " - " << level;
+  }else if(node.IsNull()){
+    MEGALO_LOG_INFO(MEGALO_LOG_ROOT()) << std::string(level*4,' ') << "NULL" << " - " << node.Type() << " - " << level;
+  }else if(node.IsMap()){
+    for(auto it = node.begin();
+        it != node.end(); ++it){
+      MEGALO_LOG_INFO(MEGALO_LOG_ROOT()) << std::string(level*4,' ') << it->first << " - " << it->second.Type() << " - " << level;
+      print_yaml(it->second,level+1);
+    }
+  }else if(node.IsSequence()){
+    for(size_t i = 0;i < node.size();++i){
+      MEGALO_LOG_INFO(MEGALO_LOG_ROOT()) << std::string(level*4,' ') << i << " - " << node[i].Type() << " - " << level;
+      print_yaml(node[i],level+1); 
+    }
+  }
+}
+
+
+void test_yaml(){
+  YAML::Node root = YAML::LoadFile("/home/mars/Project/megalo/bin/conf/test.yml");
+  print_yaml(root,0);
+
+  MEGALO_LOG_INFO(MEGALO_LOG_ROOT()) << root.Scalar();
+}
+
+void test_config(){
+  MEGALO_LOG_INFO(MEGALO_LOG_ROOT()) <<"Before:" << g_config_int_value->getValue(); 
+  MEGALO_LOG_INFO(MEGALO_LOG_ROOT()) <<"Before:" << g_config_float_value->getValue(); 
+
+
+#define XX(g_var, name, prefix) \
+  { \
+    auto& v = g_var->getValue(); \
+    for(auto& i : v){ \
+      MEGALO_LOG_INFO(MEGALO_LOG_ROOT()) << #prefix " " #name ": " << i; \
+    } \
+    MEGALO_LOG_INFO(MEGALO_LOG_ROOT()) << #prefix " " #name " yaml: " << g_var->toString(); \
+  } 
+
+#define XX_M(g_var, name, prefix) \
+  { \
+    auto& v = g_var->getValue(); \
+    for(auto& i : v){ \
+      MEGALO_LOG_INFO(MEGALO_LOG_ROOT()) << #prefix " " #name ": {" \
+            << i.first << " - " << i.second << "}"; \
+    } \
+    MEGALO_LOG_INFO(MEGALO_LOG_ROOT()) << #prefix " " #name " yaml: " << g_var->toString(); \
+  }
+
+  XX(g_config_int_vec_value, int_vec, before);
+  XX(g_config_int_list_value, int_list, before);
+  XX(g_config_int_set_value, int_set, before);
+  XX(g_config_int_uset_value, int_uset, before);
+  XX_M(g_config_str_int_map_value, str_int_map, before);
+
+  YAML::Node root = YAML::LoadFile("/home/mars/Project/megalo/bin/conf/test.yml");
+  megalo::Config::LoadFromYaml(root);
+
+  MEGALO_LOG_INFO(MEGALO_LOG_ROOT()) <<"After:" << g_config_int_value->getValue(); 
+  MEGALO_LOG_INFO(MEGALO_LOG_ROOT()) <<"After:" << g_config_float_value->getValue(); 
+
+  XX(g_config_int_vec_value, int_vec, after);
+  XX(g_config_int_list_value, int_list, after);
+  XX(g_config_int_set_value, int_set, after);
+  XX(g_config_int_uset_value, int_uset, after);
+  XX_M(g_config_str_int_map_value, str_int_map, after);
+       
+}
+
+
+class Person{
+public:
+  Person() {}
+  std::string toString() const {
+    std::stringstream ss;
+    ss << "[Person name=" << m_name 
+       << " age=" << m_age 
+       << " sex=" << m_sex 
+       << "]";
+    return ss.str();
+  }
+
+  bool operator==(const Person& other) const{
+    return m_name == other.m_name
+        && m_age == other.m_age
+        && m_sex == other.m_sex;
+  }
+  
+  std::string m_name;
+  int m_age = 0;
+  bool m_sex = 0;
+};
+
+namespace megalo{
+
+template<>
+class LexicalCast<std::string, Person>{
+public:
+  Person operator()(const std::string& v){
+    YAML::Node node = YAML::Load(v);
+    Person p;
+    p.m_name = node["name"].as<std::string>();
+    p.m_age = node["age"].as<int>();
+    p.m_sex = node["sex"].as<bool>();
+    return p;
+  }
+};
+
+
+template<>
+class LexicalCast<Person, std::string>{
+public:
+  std::string operator()(const Person& v){
+    YAML::Node node(YAML::NodeType::Map);
+    node["name"] = v.m_name;
+    node["age"] = v.m_age;
+    node["sex"] = v.m_sex;
+    std::stringstream ss;
+    ss << node;
+    return ss.str();
+  }
+};
+
+}
+
+megalo::ConfigVar<Person>::ptr g_person = 
+  megalo::Config::Lookup("class.person", Person(), "system person");
+
+megalo::ConfigVar<std::map<std::string, Person> >::ptr g_person_map = 
+  megalo::Config::Lookup("class.map", std::map<std::string, Person>{}, "system person map");
+
+void test_class(){
+
+  g_person->addListener(10,[](const Person& old_value, const Person& new_value){
+        MEGALO_LOG_INFO(MEGALO_LOG_ROOT()) << "old_value=" << old_value.toString()
+        << " new_value=" << new_value.toString();
+      });
+
+#define XX_MP(g_var, prefix) \
+  { \
+    auto& v = g_var->getValue(); \
+    for(auto& i : v){ \
+      MEGALO_LOG_INFO(MEGALO_LOG_ROOT()) << #prefix ": " << i.first << " - " << i.second.toString(); \
+    } \
+    MEGALO_LOG_INFO(MEGALO_LOG_ROOT()) << #prefix ": size=" << v.size(); \
+  }
+
+  MEGALO_LOG_INFO(MEGALO_LOG_ROOT()) << "before: " << g_person->getValue().toString() << " - " << g_person->toString();
+  XX_MP(g_person_map, class.map before);
+
+  YAML::Node root = YAML::LoadFile("/home/mars/Project/megalo/bin/conf/test.yml");
+  megalo::Config::LoadFromYaml(root);
+
+  MEGALO_LOG_INFO(MEGALO_LOG_ROOT()) << "after: " << g_person->getValue().toString() << " - " << g_person->toString();
+  XX_MP(g_person_map, class.map after);
+}
 int main(){
-  MEGALO_LOG_INFO(MEGALO_LOG_ROOT()) << g_config_int_value->getValue(); 
+  //test_yaml();
+  //test_config();
+  test_class();
   return 0;
 }
